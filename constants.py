311d1583afe3f7c6e82c787e414e6d5615aef079
wentaizhao <110541688+wentaizhao@users.noreply.github.com>
2022-08-15 10:59:31 -0400

    Create constants.py

---
+++ constants.py
@@ -0,0 +1,34 @@
+DRIVER_PATH = DRIVER_PATH
+
+BUTTONS = {
+    'Q': '//*[@id=\"wordle-app-game\"]/div[2]/div[1]/button[1]',
+    'W': '//*[@id=\"wordle-app-game\"]/div[2]/div[1]/button[2]',
+    'E': '//*[@id=\"wordle-app-game\"]/div[2]/div[1]/button[3]',
+    'R': '//*[@id=\"wordle-app-game\"]/div[2]/div[1]/button[4]',
+    'T': '//*[@id=\"wordle-app-game\"]/div[2]/div[1]/button[5]',
+    'Y': '//*[@id=\"wordle-app-game\"]/div[2]/div[1]/button[6]',
+    'U': '//*[@id=\"wordle-app-game\"]/div[2]/div[1]/button[7]',
+    'I': '//*[@id=\"wordle-app-game\"]/div[2]/div[1]/button[8]',
+    'O': '//*[@id=\"wordle-app-game\"]/div[2]/div[1]/button[9]',
+    'P': '//*[@id=\"wordle-app-game\"]/div[2]/div[1]/button[10]',
+
+    'A': '//*[@id=\"wordle-app-game\"]/div[2]/div[2]/button[1]',
+    'S': '//*[@id=\"wordle-app-game\"]/div[2]/div[2]/button[2]',
+    'D': '//*[@id=\"wordle-app-game\"]/div[2]/div[2]/button[3]',
+    'F': '//*[@id=\"wordle-app-game\"]/div[2]/div[2]/button[4]',
+    'G': '//*[@id=\"wordle-app-game\"]/div[2]/div[2]/button[5]',
+    'H': '//*[@id=\"wordle-app-game\"]/div[2]/div[2]/button[6]',
+    'J': '//*[@id=\"wordle-app-game\"]/div[2]/div[2]/button[7]',
+    'K': '//*[@id=\"wordle-app-game\"]/div[2]/div[2]/button[8]',
+    'L': '//*[@id=\"wordle-app-game\"]/div[2]/div[2]/button[9]',
+
+    'ENTER': '//*[@id=\"wordle-app-game\"]/div[2]/div[3]/button[1]',
+    'Z': '//*[@id=\"wordle-app-game\"]/div[2]/div[3]/button[2]',
+    'X': '//*[@id=\"wordle-app-game\"]/div[2]/div[3]/button[3]',
+    'C': '//*[@id=\"wordle-app-game\"]/div[2]/div[3]/button[4]',
+    'V': '//*[@id=\"wordle-app-game\"]/div[2]/div[3]/button[5]',
+    'B': '//*[@id=\"wordle-app-game\"]/div[2]/div[3]/button[6]',
+    'N': '//*[@id=\"wordle-app-game\"]/div[2]/div[3]/button[7]',
+    'M': '//*[@id=\"wordle-app-game\"]/div[2]/div[3]/button[8]',
+    'BACK': '//*[@id=\"wordle-app-game\"]/div[2]/div[3]/button[9]'
+}
